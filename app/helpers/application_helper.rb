c6d88b1b3b3a904df94d0565c598f13a87afc3fe
Jean Barbosa <jeanbarbosa9@gmail.com>
2020-09-02 12:51:34 -0300

    Helper para verificar ambiente do rails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@ module ApplicationHelper
   def format_date_br(us_date_format)
     us_date_format.strftime('%d/%m/%Y')
   end
+
+  def rails_environment
+    environments = {
+      production: 'produção',
+      development: 'desenvolvimento',
+      test: 'teste',
+      staging: 'homologação'
+    }
+
+    environments[Rails.env.to_sym]
+  end
 end
