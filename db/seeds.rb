7a799b6da29dd77bb000ceb18522b1585a6cd186
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-05-27 09:32:42 -0300

    Adiciona cadastro inicial de moedas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+initial_coins = [
+  {
+    coin_name: 'Bitcoin',
+    acronym: 'BTC',
+    image_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRewQoPbineQHwaypFEwjsWZ_2IsEtc5UWEGtjvryuxJqFngZsfSq4lbPEbrubaUeuW_T0&usqp=CAU'
+  },
+  {
+    coin_name: 'Ethereum',
+    acronym: 'ETH',
+    image_url: 'https://tl.vhv.rs/dpng/s/420-4206472_fork-cryptocurrency-ethereum-bitcoin-classic-png-download-ethereum.png'
+  },
+  {
+    coin_name: 'Dash',
+    acronym: 'DASH',
+    image_url: 'https://seeklogo.com/images/D/dash-logo-4A14989CF5-seeklogo.com.png'
+  },
+]
+
+puts 'Iniciando cadastro das moedas...'
+initial_coins.each do |coin|
+  Coin.create!(
+    coin_name: coin[:coin_name],
+    acronym: coin[:acronym],
+    image_url: coin[:image_url],
+  )
+  puts "Criada a moeda #{coin[:coin_name]}!"
+end
+puts 'Moedas cadastradas com sucesso!'
