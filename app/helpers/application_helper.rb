9f987f1a734e0bc74936be5c9d73627e1cbad76d
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-06-08 07:54:12 -0300

    Moves welcome helpers to its conventional location

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,2 @@
-module ApplicationHelper
-  def format_date_br(us_date_format)
-    us_date_format.strftime('%d/%m/%Y')
-  end
-
-  def show_language(locale)
-    locale == :en ? 'inglês dos Estados Unidos da América' : 'português do Brasil'
-  end
-
-  def rails_environment
-    environments = {
-      production: 'produção',
-      development: 'desenvolvimento',
-      test: 'teste',
-      staging: 'homologação'
-    }
-
-    environments[Rails.env.to_sym]
-  end
-end
+# Methods to help rendering infos to share with entire application
+module ApplicationHelper; end
