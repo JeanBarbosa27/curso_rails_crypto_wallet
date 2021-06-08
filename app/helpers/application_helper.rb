14ef800e567746aafc688e6ad2973e5ac3a49c62
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-06-08 07:28:42 -0300

    Handle language preference rendering

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     us_date_format.strftime('%d/%m/%Y')
   end
 
+  def show_language(locale)
+    locale == :en ? 'inglês dos Estados Unidos da América' : 'português do Brasil'
+  end
+
   def rails_environment
     environments = {
       production: 'produção',
