190f4b7aa25a8d314634a240dbb134f26a7ba365
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-06-08 09:15:57 -0300

    Translating model names into views

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @teacher = 'Jackson Pires'  
+    @teacher = 'Jackson Pires'
     @course = params[:curso] || 'Ruby on Rails 5.2 - do início ao fim'
+    @coins = Coin.all
+    @mining_types = MiningType.all
   end
 end
