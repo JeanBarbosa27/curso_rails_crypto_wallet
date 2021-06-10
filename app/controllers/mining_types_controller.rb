f9332791097f0c908537581ea6eaa9d1a080305c
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-06-10 18:51:01 -0300

    Attend to rubocop conventions

--- app/controllers/mining_types_controller.rb
+++ app/controllers/mining_types_controller.rb
@@ -8,8 +8,7 @@ class MiningTypesController < ApplicationController
   end
 
   # GET /mining_types/1 or /mining_types/1.json
-  def show
-  end
+  def show; end
 
   # GET /mining_types/new
   def new
@@ -17,8 +16,7 @@ class MiningTypesController < ApplicationController
   end
 
   # GET /mining_types/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /mining_types or /mining_types.json
   def create
@@ -26,7 +24,7 @@ class MiningTypesController < ApplicationController
 
     respond_to do |format|
       if @mining_type.save
-        format.html { redirect_to @mining_type, notice: "Mining type was successfully created." }
+        format.html { redirect_to @mining_type, notice: 'Mining type was successfully created.' }
         format.json { render :show, status: :created, location: @mining_type }
       else
         format.html { render :new, status: :unprocessable_entity }
